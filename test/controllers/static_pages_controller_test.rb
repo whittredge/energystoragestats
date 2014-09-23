46586663754970b6dbbc2b7a02cdb76096f26394
Paul B Whittredge <whittredge@gmail.com>
2014-09-23 17:58:38 -0400

    Change static_pages_controller to include base_title rb---more DRY.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+  	@base_title = "EnergyStorageStats"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | EnergyStorageStats"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | EnergyStorageStats"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | EnergyStorageStats"
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
