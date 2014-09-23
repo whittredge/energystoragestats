450417c3361ed2048b96b9e7ef5973d3c1064ffe
Paul B Whittredge <whittredge@gmail.com>
2014-09-23 10:01:52 -0400

    Minor update to SVG/D3 demo, V1.2: include untracked js file and asset folder 'data'.

---
+++ app/assets/javascripts/SVGdemo.js
@@ -0,0 +1,12 @@
+//Copied from http://www.recursion.org/d3-for-mere-mortals/
+
+var rectDemo = d3.select("#rectDemo").
+  append("svg:svg").
+  attr("width", 400).
+  attr("height", 300);
+
+rectDemo.append("svg:rect").
+  attr("x", 100).
+  attr("y", 100).
+  attr("height", 100).
+  attr("width", 200);
