050aa2d3d6fec94531a760e27d554806c7ad4730
Paul B Whittredge <whittredge@gmail.com>
2014-09-23 17:38:14 -0400

    Rework static pages according to RailsTutorial ch 3, to include ERb.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | EnergyStorageStats"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | EnergyStorageStats"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | EnergyStorageStats"
   end
 
 end
